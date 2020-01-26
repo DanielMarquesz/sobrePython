5e0a7c89e51f0f2dd6cfba0decef0f381a207fb6
Daniel Marques <daniel.marques142@gmail.com>
2020-01-26 03:34:11 -0300

    tuplas

---
+++ tuples.py
@@ -0,0 +1,31 @@
+"""
+Tuplas -
+
+São parecidas com listas, porém elas:
+
+ - São compostas por "( )"
+ - Tuplas são imutáveis: Ao se criar uma tupla ela não muda, toda operação em uma tupla gera uma nova tupla.
+
+# Exemplos de tuplas
+
+# Ex1:
+
+tupla1 = (1, 2, 3, 4, 5, 6)
+print(tupla1)
+
+print(type(tupla1))
+
+tupla2 = 1, 2, 3, 4, 5, 6
+print(tupla2)
+
+print(type(tupla2))
+
+# Ex2:
+
+tupla3 = (1)  # Tuplas são definidas pelo uso de vírdulas,ao invés de parenteses
+print(tupla3)
+
+print(type(tupla3))
+
+"""
+
