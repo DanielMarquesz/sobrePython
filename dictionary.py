2d0b3ad3f43d1454fc761f9d0d454aa9284a7272
Daniel Marques <daniel.marques142@gmail.com>
2020-02-01 03:22:10 -0300

    'attdicionarios'

---
+++ dictionary.py
@@ -0,0 +1,55 @@
+"""
+Dicionários
+
+OBS: Em algumas linguagens de programação: os dicionários Python são conhecidos por mapas.
+
+Dicionários são  coleções do tipo chave/valor.
+
+Os Dicionários são representados por colchetes. {}.
+
+print(type({})
+
+# Sobre os Dicionários
+
+ - Chave e Valor são separados por ':' - Ex : 'chave:valor'.
+
+
+paises = {'br':'Brasil','eua':'Estados Unidos','py':'Python'}
+print(paises)
+print(type(paises))
+
+# Acessando Elementos
+# Forma 1 - Acessando via chave, da mesma forma que lista/tupla
+
+print(paises['br'])
+print(paises['py'])
+
+# Forma 2 - Acessando via Get - (Recomendada)
+
+print(paises.get('br'))
+print(paises.get('eua'))
+
+# Encontrando e não encontrando chaves no dicionário
+
+paises = {'br':'Brasil','eua':'Estados Unidos','py':'Python'}
+pais = paises.get('br','Não Cadastrado') # Caso não ache o a chave, retorna um valor pré definido
+
+print(f'Encontrado: {pais}')
+# Podemos verificar se determinada chave se encontra em um dicionário
+paises = {'br':'Brasil','eua':'Estados Unidos','py':'Python'}
+
+print('br' in paises)
+print('ru' in paises)
+print('Estados Unidos' in paises)
+"""
+
+# Podemos Utilizar qualquer tipo de dado (int, float, string, boolean), inclusive, listas, tuplas,
+# dicionários como chaves de dicionários
+
+# Coordenadas de locais
+localidades = {
+    (35.3895, 39.6917): 'Escritório em Tokyo',
+    (37.3445, 12.0757): 'Escritório em Rio de Janeiro',
+    (25.6745, 19.4565): 'Escritório em São Paulo'
+}
+print(localidades)
